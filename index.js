4ba86f4b7eb40e684b354564cbb52a60e7fb0f1c
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-03-04 21:26:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function Render(temp, obj) {
+    let key
+    for (key in obj) {
+        if (typeof obj[key] === "string") {
+            temp = temp.replace(
+                new RegExp(`{{ ${key} }}`, "gm"),
+                obj[key].toString()
+            )
+        }
+    }
+    temp = temp.replace(/\{\{\ \w+\ \}\}/gmi, "")
+    return temp
+}
+
+module.exports = Render
