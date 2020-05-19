0b42de2aaec2f6fa49d22f60c1a75728c60e2c5a
Ilja B <49001925+iljucha@users.noreply.github.com>
2020-05-19 10:13:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-module.exports = Render
-
-function Render(temp, obj, opt = { removeUnused: true }) {
+/**
+ * Replaces {{ key }}s in string template
+ * @param {string} temp - String template
+ * @param {object} obj - Key-Values
+ */
+export default function Render(temp, obj) {
+    if (typeof temp !== "string") return ""
     let key
     for (key in obj) {
         if (typeof obj[key] === "string") {
             temp = temp.replace(
-                new RegExp(`{{ ${key} }}`, "gm"),
+                new RegExp(`{{ ?${key} ?}}`, "gm"),
                 obj[key].toString()
             )
         }
-    }
-    if (opt.removeUnused == true) {
-        temp = temp.replace(/\{\{\ ?\w+\.?\w+\ ?\}\}/gmi, "")
     }
     return temp
 }
